e8b35d1b21bb9c9fc685acc09f6341ace88a903a
Tony Tan <tanxiucai1@gmail.com>
2017-07-16 21:56:31 +0800

    animate route transactions

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
     <div class="container">
         <app-header></app-header>
         <div class="col-md-12">
-            <router-view></router-view>
+    
+            <transition name="slide" mode="out-in">
+                <router-view></router-view>
+            </transition>
         </div>
     </div>
 </template>
@@ -20,5 +23,37 @@ export default {
 </script>
 
 <style>
+body {
+    padding: 30px
+}
 
+.slide-enter-active {
+    animation: slide-in 200ms ease-out forwards;
+}
+
+.slide-leave-active {
+        animation: slide-out 200ms ease-out forwards;
+}
+
+@keyframes slide-in {
+    from {
+        transform: translateY(-30px);
+        opacity: 0;
+    }
+    to {
+        transform: translateY(0);
+        opacity: 0;
+    }
+}
+
+@keyframes slide-in {
+    from {
+        transform: translateY(0);
+        opacity: 1;
+    }
+    to {
+        transform: translateY(-30px);
+        opacity: 0;
+    }
+}
 </style>
